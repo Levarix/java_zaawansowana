b31d7cfce05de913522c5388da9a2c4053bc8f85
Maciej Rybacki <rybackimaciej03@gmail.com>
2020-03-09 11:38:19 +0100

    Prepared the solution for exercise 1 with calculate avg

--- app/src/main/java/pl/jazapp/app/AverageServlet.java
+++ app/src/main/java/pl/jazapp/app/AverageServlet.java
@@ -1,7 +1,42 @@
 package pl.jazapp.app;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+@WebServlet("average")
+public class AverageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setStatus(200);
+        resp.setContentType("text/plain");
+
+        var numbers = req.getParameter("numbers");
+        double sum = 0;
+        double avg;
+
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
+        df.setMinimumFractionDigits(0);
+
+        String[] splitedNumbers = numbers.split(",");
+
+        var writer = resp.getWriter();
+
+        try {
+            writer.println("I got '" + numbers + "'");
+            for (int i = 0; i < splitedNumbers.length; i++){
+                sum += Double.parseDouble(splitedNumbers[i]);
+            }
+            avg = sum/splitedNumbers.length;
+            writer.println("Average equals: " + df.format(avg));
+        } catch (NumberFormatException e) {
+            writer.println("Haven't given any parameter to calculate the average");
+        }
+
     }
 }
